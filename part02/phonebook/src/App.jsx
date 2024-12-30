26e89b5fc557f01a1a20154210756847632dc0ac
M Lincon <49597604+mlincon@users.noreply.github.com>
2024-12-30 18:44:37 +0000

    part02 forms exercise

---
+++ part02/phonebook/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const Person = ({ person }) => {
+  return <p>{person.name}</p>;
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    const personObject = { name: newName };
+    setPersons(persons.concat(personObject));
+  };
+
+  const handleAddPerson = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input onChange={handleAddPerson} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <Person key={person.name} person={person} />
+      ))}
+      <div>debug: {newName}</div>
+    </div>
+  );
+};
+
+export default App;
