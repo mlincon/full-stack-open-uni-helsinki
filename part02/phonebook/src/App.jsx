ff2650c3d725f6b72e25e602f228e8ac1f4e57c7
M Lincon <49597604+mlincon@users.noreply.github.com>
2025-01-03 11:40:34 +0000

    ex. 2.9 part1

--- part02/phonebook/src/App.jsx
+++ part02/phonebook/src/App.jsx
@@ -14,10 +14,14 @@ const checkDuplicate = (people, name) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1243567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [showAll, setShowAll] = useState(true);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -39,9 +43,20 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const peopleToShow = showAll
+    ? persons
+    : persons.filter((person) =>
+        person.name.toLowerCase().includes(search.toLowerCase())
+      );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={() => "hello"} />
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange={handleAddName} />
@@ -53,11 +68,11 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {peopleToShow.map((person) => (
         <Person key={person.name} person={person} />
       ))}
-      <div>debug: {newName}</div>
     </div>
   );
 };
