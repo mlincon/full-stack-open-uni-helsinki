b2e979fe59e3debcbf377fe04c0b077a2a3a1314
M Lincon <49597604+mlincon@users.noreply.github.com>
2025-01-08 11:35:52 +0000

    ex. 2.14 confirmation delete

--- part02/phonebook/src/components/Numbers.jsx
+++ part02/phonebook/src/components/Numbers.jsx
@@ -1,16 +1,18 @@
 import crud from "../services/crude";
 
 const Person = ({ person, setPersons }) => {
-  const deletePerson = (id) => {
-    crud.remove(id).then((data) => {
-      console.log(data);
-    });
+  const deletePerson = (id, name) => {
+    if(window.confirm(`Delete ${name}?`)) {
+      crud.remove(id).then((data) => {
+        console.log(data)
+      });
+    }
   };
 
   return (
     <p>
       {person.name}: {person.number}{" "}
-      <button onClick={() => deletePerson(person.id)}>delete</button>
+      <button onClick={() => deletePerson(person.id, person.name)}>delete</button>
     </p>
   );
 };
