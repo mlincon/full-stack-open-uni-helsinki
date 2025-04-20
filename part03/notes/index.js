ae8086554307d32110cbe9cc6e746a7829c75674
M Lincon <49597604+mlincon@users.noreply.github.com>
2025-04-20 21:42:50 +0000

    part03a

--- part03/notes/index.js
+++ part03/notes/index.js
@@ -19,6 +19,8 @@ let notes = [
   },
 ];
 
+app.use(express.json()); // json-parser middleware to render request.body properly
+
 app.get("/", (request, response) => {
   // event handler
   response.send("<h1>Hello World!</h1>");
@@ -37,6 +39,32 @@ app.get("/api/notes/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(notes);
+
+  response.json(note);
 });
 
 app.delete("/api/notes/:id", (request, response) => {
